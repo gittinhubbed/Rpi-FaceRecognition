b91bbddf2d5c63ced9e53a7518b20fefc4509a88
Joe Graber <gurney.buchanan@gmail.com>
2019-04-30 17:08:01 +0000

    update

--- lum.py
+++ lum.py
@@ -8,28 +8,6 @@ import RPi.GPIO as GPIO
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv.imread('sys.argv')
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-	cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-	roi_gray = gray[y:y+h, x:x+w]
-	roi_color = gray[y:y+h, x:x+w]
-	eyes = eye_cascade.detectMultiScale(roi_gray)
-	for (ex,ey,ew,eh) in eyes:
-		cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-	
-cv.imshow('img',img)
-cv.waitKey(0)
-cv.destroyAllWindows()
-
-
-
-
-
-
-
 GPIO.setmode(GPIO.BOARD)
 PIR_PIN = 16
 GPIO.setup(PIR_PIN, GPIO.IN)
@@ -43,10 +21,24 @@ try:
     i = 0
     while True:
         if GPIO.input(PIR_PIN):
-            with open("tem_vid" + str(i) + ".h264", "w") as file:
+            with open("" + str(i) + ".jpg", "w") as file:
                 camera.capture(file.name)
-                
-                mot = 0
+                img = cv.imread(file.name)
+                gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+                faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+                for (x,y,w,h) in faces:
+	                cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+	                roi_gray = gray[y:y+h, x:x+w]
+	                roi_color = gray[y:y+h, x:x+w]
+	                eyes = eye_cascade.detectMultiScale(roi_gray)
+	                for (ex,ey,ew,eh) in eyes:
+		                cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+                cv.destroyAllWindows()
+                cv.imshow('img',img)
+                sleep(10)
+                cv.waitKey(0)
+
                 for x in range(20):
                     if (GPIO.input(PIR_PIN)):
                         mot += 1
